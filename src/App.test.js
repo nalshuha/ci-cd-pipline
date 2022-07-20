62ea9ebbf3d2265cb979cd427537761670c0ddfc
nalshuha <93419895+nalshuha@users.noreply.github.com>
2022-07-20 10:21:53 +0300

    change test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn js/i);
+  const linkElement = screen.getByText(/learn React/i);
   expect(linkElement).toBeInTheDocument();
 });
